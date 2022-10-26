040267ac806e27aca8e05843c5222c522c47a0a4
= <=>
2022-10-27 01:53:38 +0300

    fdf

--- data_information.py
+++ data_information.py
@@ -1,29 +1,36 @@
 import os
-import cv2
-import json
-import numpy as np
+import pickle
 
-def get_data_information(directory_path: str) -> None:
-    """Функция, которая выводит количество классов,
-     а также количество изображений (объектов)"""
+PROCESSED_IMAGES_PATH = '/Users/alisultanov/Desktop/Обработанные фотки'
+
+def get_information(directory_path: str) -> None:
+    """A function that prints the number of classes,
+     as well as the number of images (objects)"""
     directories = os.listdir(directory_path)
     if '.DS_Store' in directories:
         directories.remove('.DS_Store')
-    print(f'Количество классов: {len(directories)}')
+    print(f'Number of classes: {len(directories)}')
     image_count = 0
     for directory in directories:
         path = directory_path + '/' + directory
-        for im in os.listdir(path):  # пробегаемся по названию изображени
-            if im == '.DS_Store':  # удаляем из подсчёта скрытый файл .DS_Store
+        for img_path in os.listdir(path):  # scrolling through the title of the image
+            if img_path == '.DS_Store':  # remove the hidden .DS_Store file from the count
                 continue
-            if os.path.isfile(path + '/' + im):
+            if os.path.isfile(path + '/' + img_path):
                 image_count += 1
-    print(f'Количество изображений (объектов): {image_count}')
+    print(f'Number of images (objects): {image_count}')
+
+
+
+def get_dataset_information(dataset_path: str) -> None:
+    """A function that displays information about the size of the dataset
+    dataset_path: embeddings.pkl or labels.pkl"""
+    with open(dataset_path, 'rb') as file:
+        dataset = pickle.load(file)
+        print(f'Dataset size: {len(dataset)} objects')
 
 
 
 if __name__ == '__main__':
-    get_data_information('/Users/alisultanov/Desktop/Обработанные фотки')
-
-    
-dfsdfsd
+    get_information(PROCESSED_IMAGES_PATH)
+    get_dataset_information('embeddings.pkl')
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,16 +5,19 @@ import numpy as np
 import cv2
 import face_recognition
 
+PROCESSED_IMAGES_PATH = '/Users/alisultanov/Desktop/Обработанные фотки'
+
+
 def get_path(directory_path) -> list:
-    """Функция, которая получает ссылки на все существующие изображения"""
+    """A function that gets links to all existing images"""
     img_path_list = []
     directories = os.listdir(directory_path)
     if '.DS_Store' in directories:
         directories.remove('.DS_Store')
     for directory in directories:
         path = directory_path + '/' + directory
-        for im in os.listdir(path):  # пробегаемся по названию изображений. im - это конкретное изображение
-            if im == '.DS_Store':  # удаляем из подсчёта скрытый файл .DS_Store
+        for im in os.listdir(path):  # we run through the names of the images,  im is a specific image
+            if im == '.DS_Store':  # we remove the hidden file from the calculation .DS_Store
                 continue
             image_path = path + '/' + im
             if os.path.isfile(image_path):
@@ -24,32 +27,32 @@ def get_path(directory_path) -> list:
 
 
 def image_resize(img: np.ndarray, width: int | float, height: int | float) -> np.ndarray:
-    """Функция, которая изменяет размер изображений лица до стандартного размера"""
+    """A function that resizes face images to a standard size"""
     return cv2.resize(img, (width, height))
 
 
 def image_save(img: np.ndarray, save_img_path: str) -> None:
-    """Функция, которая сохраняет изображение"""
+    """A function that saves an image"""
     cv2.imwrite(save_img_path, img)
 
 
-# Todo: Получили эмбеддинги, успешно сохранили из в pickle
-
 def face_detect(directory_path: str, new_directory_path: str) -> None:
-    """Функция, которая отвечает за распознавание лиц на изображениях,
-    вызывает ф-ии изменения размера изображения и сохранения в новой директории"""
+    """The function that is responsible for recognizing faces in images,
+    causes the image to be resized and saved in a new directory"""
     img_path_list = get_path(directory_path)
     for img_path in img_path_list:
-        img = cv2.imread(img_path)  # здесь хранится тип np.ndarray
+        img = cv2.imread(img_path)  # the np.ndarray type is stored here
         face_location = face_recognition.face_locations(img)
         if len(face_location) == 0:
-            print(f'Не обнаружено лицо на изображении: {img_path}')
-        if len(face_location) == 1:
+            print(f'No face detected in the image: {img_path}')
+        elif len(face_location) > 1:
+            print(f'Several faces have been detected in the image: {img_path}')
+        else:
             try:
                 width = face_location[0][1] - face_location[0][3]
                 height = face_location[0][2] - face_location[0][0]
                 img = img[face_location[0][0] - height // 3: face_location[0][0] + height, face_location[0][3]: face_location[0][3] + width]
-                img = image_resize(img, 170, 170)  # Размер изображения согласован с архитектурой CNN
+                img = image_resize(img, 170, 170)
 
                 name = img_path[42: img_path.rfind('/')]
                 name_path = new_directory_path + '/' + name
@@ -57,88 +60,70 @@ def face_detect(directory_path: str, new_directory_path: str) -> None:
                     os.mkdir(name_path)
                 new_img_path = name_path + img_path[img_path.rfind('/'):]
                 image_save(img, new_img_path)
-                os.remove(img_path)  # удаляем изображение из старого файла (для удобства обработки в дальнейшем)
+                os.remove(img_path)  # we delete the image from the old file
 
-            except cv2.error:  # проблема вылезает из-за того, что face_recog выдает [], т.е len == 1 - заходим в этот блок, а матрицы изобр - нету. Проблема свелась к ручной обработке изображения
-                print(f'Ошибка вылезла здесь: {img_path}')
+            except cv2.error:
+                print(f'Image processing error: {img_path}')
         if len(face_location) > 1:
-            print(f'Обнаружено несколько лиц на изображении: {img_path}')
+            print(f'Several faces have been detected in the image: {img_path}')
 
 
 def image_replace(directory_path: str) -> None:
-    """Функция по переносу изображения из директории img_path в final_img_path"""
+    """Function for transferring an image from the img_path directory to final_img_path"""
     img_path_list = get_path(directory_path)
     for img_path in img_path_list:
         try:
             new_img_path = img_path.replace('Celebrities(2)', 'Обработанные фотки')
             shutil.move(img_path, new_img_path)
         except FileNotFoundError:
-            print(f'Не существует директории для этого изображения: {img_path}')
+            print(f'There is no directory for this image: {img_path}')
 
 
 def get_id(directory_path: str) -> dict:
-    """Функция, которая по каждому человеку получает id значение и сохраняет в словарь"""
+    """A function that for each person gets an id value and saves it to a dictionary"""
     counter = 0
     id_dict = {}
     persons = os.listdir(directory_path)
     for person in persons:
+        if person == '.DS_Store':
+            continue
         if person not in id_dict:
             id_dict[person] = counter
             counter += 1
         else:
             continue
+    id_dict = {str(value): key for key, value in id_dict.items()}
     return id_dict
 
 
-id_dict = get_id('/Users/alisultanov/Desktop/Обработанные фотки')
-print(id_dict)
+id_person = get_id(PROCESSED_IMAGES_PATH)
 
-def get_embeddings(directory_path: str) -> (np.array, np.array):
-    """Функция получения представления изображений в цифровом виде"""
-    X_data = []
+
+def get_embeddings_and_labels(directory_path: str) -> (np.array, np.array):
+    """Function to get embeddings and class tags for all images from a directory"""
+    X_data = np.empty(128)
     y_data = []
     img_path_list = get_path(directory_path)
-    for img_path in img_path_list:  # находимся на конкретном изображении из директории
-        img_tensor = cv2.imread(img_path)
-        X_data.append(img_tensor)  # добавили цифровое представление изображения
+    for img_path in img_path_list:  # we are on a specific image from the directory
+        img = cv2.imread(img_path)
+        img = face_recognition.face_encodings(img)
+        if not img:
+            continue
+        X_data = np.vstack((X_data, img))  # added a digital representation of the image
         name = img_path[46: img_path.rfind('/')]
-        y_data.append(id_dict[name])  # добавили метку класса
-
+        y_data.append(id_person[name])  # added a class label
+    X_data = X_data[1:]
     return np.array(X_data), np.array(y_data)
 
 
-X, y = get_embeddings('/Users/alisultanov/Desktop/Обработанные фотки')
-
-
-
 def pickle_save(file_name: str, data: np.array) -> None:
-    """Функция сохранения эмбеддингов в формате .pkl
-    (используется как для X, так и для y)"""
+    """The function of saving embeddings in .pcl format
+       (used for both X and y)"""
     with open(file_name, 'wb') as f:
         pickle.dump(data, f, protocol=5)
 
 
-# pickle_save('embedings.pkl', X)
-# pickle_save('labels.pkl', y)
-
-
-with open('embedings.pkl', 'rb') as f_1:
-    embd = pickle.load(f_1)
-
-# with open('labels.pkl', 'rb') as f_2:
-#     lab = pickle.load(f_2)
-
-
-print(embd[0])
-
-# cv2.imshow(f'{lab[0]}', embd[0])
-# cv2.waitKey(0)
-
-
-# if __name__ == '__main__':
-#     global_resize('/Users/alisultanov/Desktop/Обработанные фотки')
-#     image_replace('/Users/alisultanov/Desktop/Celebrities(2)', '/Users/alisultanov/Desktop/Обработанные фотки')
-    # face_detect('/Users/alisultanov/Desktop/Celebrities(2)', '/Users/alisultanov/Desktop/Обработанные фотки')
-    # image_resize('/Users/alisultanov/Desktop/Celebrities/Gal Gadot/img505.jpg', 500, 500)
-    # get_data_information('/Users/alisultanov/Desktop/Celebrities')
-    # print(get_path('/Users/alisultanov/Desktop/Celebrities'))
+if __name__ == '__main__':
+    X, y = get_embeddings_and_labels(PROCESSED_IMAGES_PATH)
+    pickle_save('embeddings.pkl', X)
+    pickle_save('labels.pkl', y)
