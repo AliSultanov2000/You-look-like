38db00658cca0654e55054a05964c994711b07fe
Ali Sultanov <alishka.cultanov@bk.ru>
2023-02-18 23:38:37 +0300

    Update data_parsing.py

--- data_parsing.py
+++ data_parsing.py
@@ -76,25 +76,7 @@ def save_image(urls: set) -> None:
         time.sleep(0.05)
 
 
-other_celebrities = ['alla_pugacheva', 'charlie_hunnam', 'jo_In_seong', 'channing_tatum',  'constance_wu',
-                     'cee_lo_green', 'christian_bale', 'chris_evans', 'cillian_murphy', 'colin_farrel', 'bella_hadid', 'barack_obama',
-                     'bradley_cooper', 'bred_pitt', 'bruce_willis', 'dakota_johnson',  'pitbull', 'shia_labeouf', 'kit_harington',
-                     'nargis_fakhri', 'kate_clapp',  'alison_sudol', 'james_franco', 'jake_gyllenhaal', 'jamie_dornan', 'renee_lacombe', 'robbie_willams',
-                     'jensen_ackles', 'jesse_eisenberg', 'jim_parsons', 'justin_timberlake', 'justin_bieber', 'lindsay_ellingson',
-                     'leonardo_dicaprio', 'luke_evans', 'luis_fonsi', 'orlando_bloom', 'venus_williams', 'robert_downey_jr', 'vin_diesel',
-                     'taraji_p._henson', 'tom_cruz', 'tobias_sorensen', 'tom_holland', 'tom_hardy', 'tom_welling', 'tupac_shakur', 'tobey_maguire',
-                     'til_schweiger', 'zayn_malik', 'zac_efron', 'heath_ledger', 'xabi_alonso', 'will_smith', 'wilmer_valderrama',
-                     'alek_wek', 'anna_kendrick', 'anna_semenovich', 'aiana_grande', 'ashanti', 'arlenis_sosa_pena', 'aaron_paul', 'al_pacino', 'alain_delon',
-                     'ansel_elgort', 'antonio_banderas', 'andrey_arshavin', 'aleksey_chadov', 'aaron_johnson', 'aoi_miyazaki', 'alexa_bliss', 'bae_suzy', 'brighton_sharbino',
-                      'ben_affleck', 'camille_hurel', 'cassie', 'christina_chiabotto', 'chulpan_khamatova', 'chris_hemsworth', 'chase_tang', 'charlie_sheen', 'michael_jfox',
-                      'chris_odonnell', 'colin_morgan', 'cam_gigandet', 'callan_mcAuliffe', 'dima_nosov', 'david_villa', 'jessica_perez', 'jade_weber',
-                     'emile_hirsch', 'emilia_fox', 'emily_browning', 'edward_furlong', 'sofia_vergara', 'janina_schiedlofski', 'james_norton', 'james_purefoy', 'lisa_edelstein',
-                     'paul_walker', 'paul_wesley',  'Priyanka Chopra', 'zachary_quinto', 'zara', 'zaira_nara', 'zara_larsson', 'zendaya', 'zinadin_zidan',
-                     'zach_braff', 'hugh_jackman', 'taron_egerton', 'tessa_thompson', 'salma_hayek', 'sarah_gadon', 'owen_wilson', 'lionel_messi', 'harry_styles',
-                     'valeriy_meladze', 'arnold_schwarzenegger', 'aleksandra_bortich', 'andres_segura', 'andy_garcia', 'aaron_eckhart', 'ben_barnes', 'bow_wow',
-                     'benjamin_bratt', 'brendon_urie', 'brandon_routh', 'brenton_thwaites', 'robin_williams', 'robert_pattinson', 'robin_thicke', 'roma_zver',
-                     'roman_pavlyuchenko', 'rowan_atkinson', 'ruben_cortada', 'ryan_gosling', 'steven_mcqueen', 'Suriya', 'shawn_mendes', 'shane_west',
-                     'snoop_dogg', 'peter_gallagher', 'pedro_pascal', 'samuel_smith']
+other_celebrities = []
 
 
 if __name__ == '__main__':
